c4de20faaaa55008ef151fb692f84a0fd72cb606
carz2 <carz8500@gmail.com>
2012-01-31 12:44:59 -0700

    add device specific settings Conflicts:
    	AndroidManifest.xml
    
    Conflicts:
    
    	src/com/android/settings/Settings.java
    
    Change-Id: I048295024066bb3eedfb97fd0782e3d7f820aa2b

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -422,7 +422,12 @@ public class Settings extends PreferenceActivity
             Header header = target.get(i);
             // Ids are integers, so downcasting
             int id = (int) header.id;
-            if (id == R.id.operator_settings || id == R.id.manufacturer_settings) {
+
+            if (id == R.id.dock_settings) {
+                if (!needsDockSettings())
+                    target.remove(header);
+            } else if (id == R.id.operator_settings || id == R.id.manufacturer_settings ||
+                    id == R.id.device_settings) {
                 Utils.updateHeaderToSpecificActivityFromMetaDataOrRemove(this, target, header);
             } else if (id == R.id.wifi_settings) {
                 // Remove WiFi Settings if WiFi service is not available.
